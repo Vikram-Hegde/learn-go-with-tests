13eee653e115cdad316e95a1e3b36d21cb2080b6
Vikram <vikramhegde1440@gmail.com>
2024-09-07 20:07:48 +0530

    learnt about structs

---
+++ structure/main.go
@@ -0,0 +1,20 @@
+package structs
+
+import "fmt"
+
+type Rectangle struct {
+	Width float64
+	Height float64
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
+}
+
+func main() {
+	fmt.Println("hello world")
+}
