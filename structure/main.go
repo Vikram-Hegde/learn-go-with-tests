43e79fcfa930a941728967f816bc623b4328ae53
Vikram <vikramhegde1440@gmail.com>
2024-09-14 22:58:34 +0530

    refactored structs

--- structure/main.go
+++ structure/main.go
@@ -1,10 +1,29 @@
 package structs
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+type Shape interface {
+	Area() float64
+}
 
 type Rectangle struct {
 	Width float64
 	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
 
 func Perimeter(rectangle Rectangle) float64 {
