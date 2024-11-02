c10efc1919d0866c367c0d972b363139ed571c90
Vikram <vikramhegde1440@gmail.com>
2024-11-02 10:24:29 +0530

    progress with structs

--- structure/main.go
+++ structure/main.go
@@ -10,12 +10,17 @@ type Shape interface {
 }
 
 type Rectangle struct {
-	Width float64
+	Width  float64
 	Height float64
 }
 
 type Circle struct {
 	Radius float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
 }
 
 func (r Rectangle) Area() float64 {
@@ -24,6 +29,10 @@ func (r Rectangle) Area() float64 {
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
 }
 
 func Perimeter(rectangle Rectangle) float64 {
