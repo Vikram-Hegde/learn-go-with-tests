e7a1d401bff0aa3edc7985d538062d280d988bb7
Vikram-Hegde <vikramhegde1440@gmail.com>
2025-01-18 10:53:02 +0530

    📖 hello go

---
+++ 1. Hello, World/go.mod
@@ -0,0 +1,3 @@
+module github.com/vikram-hegde/learn-go-with-tests/hello-world
+
+go 1.23.4
--- iteration/iterate.go
+++
@@ -1,8 +0,0 @@
-package iteration
-
-func Repeat(str string, count int) (repeated string) {
-	for i := 0; i < count; i++ {
-		repeated += str
-	}
-	return 
-}
