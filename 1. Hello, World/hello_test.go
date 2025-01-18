2b12e2777ca2d7c174eda777e8590badbe54337e
Vikram-Hegde <vikramhegde1440@gmail.com>
2025-01-18 11:09:57 +0530

    ♻️ refacored tests in hello world

--- 1. Hello, World/hello_test.go
+++ 1. Hello, World/hello_test.go
@@ -4,11 +4,24 @@ import (
 	"testing"
 )
 
-func TestHello(t *testing.T) {
-	got := Hello("Vikram")
-	want := "Hello, Vikram"
-
+func assert(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf("got %q wanted %q", got, want)
 	}
+}
+
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Vikram")
+		want := "Hello, Vikram"
+
+		assert(t, got, want)
+	})
+
+	t.Run("saying hello world world by default", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+		assert(t, got, want)
+	})
 }
