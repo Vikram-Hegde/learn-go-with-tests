b818749468e4b029297590ce3494cc249abed7aa
Vikram-Hegde <vikramhegde1440@gmail.com>
2025-01-19 02:06:54 +0530

    📖 arrays and slices

---
+++ 4-Arrays_and_Slices/sum_test.go
@@ -0,0 +1,39 @@
+package arraysandslices
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSum(t *testing.T) {
+	t.Run("sum using arrays", func(t *testing.T) {
+		numbers := [5]int{1, 2, 3, 4, 5}
+
+		got := Sum(numbers)
+		want := 15
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
+	t.Run("sum using slices", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+
+		got := SumSlice(numbers)
+		want := 15
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
